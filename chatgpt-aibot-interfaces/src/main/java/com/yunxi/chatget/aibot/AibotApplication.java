65ab31c27e9634f5375a51132ff8765cf215def6
yunxi <3112278783@qq.com>
2024-03-05 22:02:18 +0800

    api

---
+++ chatgpt-aibot-interfaces/src/main/java/com/yunxi/chatget/aibot/AibotApplication.java
@@ -0,0 +1,19 @@
+package com.yunxi.chatget.aibot.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * [一句话描述该类的功能]
+ *
+ * @author : [昀息]
+ * @version : [v1.0]
+ * @createTime : [2024/3/5 20:22]
+ */
+
+@SpringBootApplication
+public class AibotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AibotApplication.class, args);
+    }
+}
